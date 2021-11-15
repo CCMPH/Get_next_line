8f8bb234581b29b3fe36c8bb1b1c9eda64ac664f
CCMPH <chartema@student.codam.nl>
2021-11-15 15:52:24 +0100

    init

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   get_next_line.h                                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: chartema <chartema@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/11/09 09:22:14 by chartema      #+#    #+#                 */
+/*   Updated: 2021/11/15 09:34:13 by chartema      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+// Moet dit onderstaande stukje wel of niet?
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+#endif
